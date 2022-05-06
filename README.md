91016ba94487975eb57492d1fc70bd4cdeb4543e
quenchaman <valericfbg@gmail.com>
2022-05-06 11:11:51 +0300

    explaining ray direction on viewport, lerp and everything so far

--- README.md
+++ README.md
@@ -1,27 +1,23 @@
 ## Ray Tracer
 
 ### Determinant of a matrix.
-If we represent a transformation using a matrix, the determinant of that matrix is the factor by which the area of the parallelogram
-is scaled after applying the transformation to the vectors.
+Determinant of two vectors A an B is the area of the parallelogram that is created when a copy of A is put at the end of B and same is done for the other side.
 
-// TODO: Add picture for explanation on why this formula is the way determinant is computed.
+![determinant explanation of formula](https://github.com/pancanin/RayTracer/blob/master/helpful_materials/matrix-determinant.PNG?raw=true)
 
 The computation for a 2D transformation is as follows:
+```
 T = [
 	a, b
 	c, d
 ]
 
 det(T) = ad - cb
+```
 
-In 3D it is a bit trickier to understand the formula, but it can be explained visually and in effect we boil it down to finding 2D determinants.
-Good way to visualise the steps needed is to create a 3x3 matrix that will help us see the operations needed.
+For 3D:
 
-
-### Cross product in 3D
-
-The cross product of two 3D vectors a and b returns a new vector that is perpendicular to a and b.
-Finding the cross product vector of two vectors in 3D space means finding the determinant of a 3x3 matrix, where:
+```
 M = [
 	i, j, k
 	ax, ay, az
@@ -30,6 +26,13 @@ M = [
 
 det(M) = det(ay, az | by, bz)i - det(ax, az | bx, bz)j + det(ax, ay | bx, by)k =
 = (ay*bz - az*by)i - (ax*bz - az*bx)j + (ax*by - ay*bx)k
+```
+
+
+### Cross product in 3D
+
+The cross product of two 3D vectors a and b returns a new vector that is perpendicular to a and b.
+Finding the cross product vector of two vectors in 3D space means finding the determinant of a 3x3 matrix, where:
 
 The result is a 3D vector (factors for i, j, k) that is the cross product of A and B.
 
@@ -37,11 +40,12 @@ Interesting fact!!
 The length of the cross product vector is equal to the determinant of A and B. :O
 
 For example, if we have vectors:
-
+```
 A = [3, 5, -7], B = [2, -6, 4]
-
+```
 We can compute the cross product with the help of a matrix.
 
+```
 A x B = [
 	i, j, k
 	3, 5, -7
@@ -57,3 +61,17 @@ Now we find the determinants of the 2D matrices.
 = -22i - 26j - 28k
 
 So the vector that is perpendicular to A and B is [-22, -26, -28]
+```
+
+### Subtracting matrices (in calculating lower left point context)
+
+Subtraction of two matrices of equal dimensions is the subtraction of corresponding elements. The result is a matrix with 
+same dimensions as the terms.
+
+It looks like subtracting two vectors gives a vector that is in the opposite or perpendicular direction of their addition vector.
+
+The lower left point is used as a reference vector to which we add the vertical and horizontal vectors adjusted with offset u, v.
+
+// TODO: Add drawing
+
+### Calculating ray direction based on current pixel
--- RayTracer.cpp
+++ RayTracer.cpp
@@ -6,10 +6,21 @@
 #include "vec3.h"
 #include "ray.h"
 
+// Linear interpolation
+vec3 lerp(const vec3& start, const vec3& end, double t) {
+    return (1.0 - t) * start + t * end;
+}
+
 color ray_color(const ray& r) {
+    // Get a normalized vector of the direction the ray is pointing to.
     vec3 unit_direction = unitVector(r.direction());
+
+    // Calculate a step for the linear interpolation based on y.
+    // The constants around y just make sure the step is finer for a softer gradient.
     auto t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+
+    // We linearly interpolate between white and bluish color.
+    return lerp(color(1.0, 1.0, 1.0), color(0.5, 0.7, 1.0), t);
 }
 
 int main()
@@ -26,19 +37,52 @@ int main()
     auto origin = point3(0, 0, 0);
     auto horizontal = vec3(viewportWidth, 0, 0);
     auto vertical = vec3(0, viewportHeight, 0);
+
+    // see image in readme about calculating lower left point.
     auto lowerLeftCorner = origin / 2 - horizontal / 2 - vertical / 2 - vec3(0, 0, focalLenght);
+
+    std::cerr << "Lower left corner calculation: \n" <<
+        "- origin is: " << origin << '\n' <<
+        "- horizontal is: " << (horizontal / 2) << '\n' <<
+        "- vertical is: " << (vertical / 2) << '\n' <<
+        "- focal length vec is: " << vec3(0, 0, focalLenght) << '\n' <<
+        "Lower left corner is " << lowerLeftCorner << '\n' << std::flush;
+
+    std::cerr << lowerLeftCorner << '\n' << std::flush;
     // Render
     std::cout << "P3" << std::endl << imageWidth << " " << imageHeight << std::endl << "255" << std::endl;
 
     for (int j = imageHeight - 1; j >= 0; --j) {
         //std::cerr << "Scanlines remaining: " << j << ' \n' << std::flush;
         for (int i = 0; i < imageWidth; ++i) {
+
+            // Normalise values of u and v.
             double u = double(i) / (imageWidth - 1);
             double v = double(j) / (imageHeight - 1);
             
-            vec3 direct(lowerLeftCorner + u * horizontal + v * vertical - origin);
-            std::cerr << direct << std::endl;
-            ray r(origin, direct);
+            // Calculate a direction vector for the ray.
+            // By multiplying the horizontal and vertical vectors with the normalized u, v values
+            // we find the value of the current component (x, y).
+            // For example: in start of the loop we got:
+            // j = 254, i = 0, imageHeight = 255, imageWidth = 400
+            // v = j / (imageHeight - 1), v = 254 / (255 - 1), v = 1
+            // u = i / (imageWidth - 1), u = 0
+
+            // v = 1, u = 0
+            // horizontal = (4, 0, 0), vertical = (0, 2, 0)
+            // horizontal * u = (4, 0, 0) * 0 = (0, 0, 0)
+            // vertical * v = (0, 2, 0) * 1 = (0, 2, 0)
+
+            // For our viewport sizes, we calculated that the lower left corner is pointed by a vector
+            // from origin to (-1.77, -1, -1).
+            // So the direction of the first ray that we will cast will be:
+            // dir = (-1.77, -1, -1) + (0, 0, 0) + (0, 2, 0) - (0, 0, 0)
+            // dir = (-1.77, 1, -1)
+
+            // On the second iteration i = 1, 'u' will become > 0 and we will get a direction slightly to the right.
+            vec3 direction(lowerLeftCorner + u * horizontal + v * vertical - origin);
+            //std::cerr << direct << std::endl;
+            ray r(origin, direction);
             color pixelColor = ray_color(r);
 
             write_color(std::cout, pixelColor);
